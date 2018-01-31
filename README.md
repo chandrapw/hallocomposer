1d7155a82102c453d0070ba90154d0161af2ae34
chandra <chandraputrawijaya@hotmail.com>
2018-01-31 15:06:05 +0700

    update lahhh

---
+++ README.md
@@ -0,0 +1,3 @@
+Project awal
+
+Membuat package di composer
---
+++ src/chandrapw/Hallocomposer/woi.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace chandrapw\hallocomposer;
+
+class woi{
+	public static function world(){
+		return 'Hello Composer!';
+	}
+}
